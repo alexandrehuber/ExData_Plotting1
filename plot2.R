8667b838a0528f4aa7348d5fd23e7e7b9f7e988c
alexandrehuber <alexandre.huber@outlook.com>
2016-10-17 11:47:25 +0200

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -9,6 +9,6 @@ d <- d[d$Date %in% c("1/2/2007", "2/2/2007"), ]
 d$DateTime <- parse_date_time(paste(d$Date, d$Time), "dmyhms")
 d$Global_active_power <- as.numeric(d$Global_active_power)
 
-png(filename = "plot2.png")
+png(filename = "plot2.png", width=480, height=480, units="px")
 plot(d$DateTime, d$Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)", xlab = "")
 dev.off()
