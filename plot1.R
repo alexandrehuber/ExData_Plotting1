bff77dba6808cf964d45b18c34ef144f22566e6d
alexandrehuber <alexandre.huber@outlook.com>
2016-10-17 11:47:01 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -6,6 +6,6 @@ d <- d[d$Date %in% c("1/2/2007", "2/2/2007"), ]
 
 d$Global_active_power <- as.numeric(d$Global_active_power)
 
-png(filename = "plot1.png")
+png(filename = "plot1.png", width=480, height=480, units="px")
 hist(d$Global_active_power, breaks = 15, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
 dev.off()
